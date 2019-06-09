e421ed19f392baeffd07d3e5c7f797e78206d9ab
parrottjt <47727512+parrottjt@users.noreply.github.com>
2019-06-09 14:55:29 -0400

    Added to Portfolio

---
+++ Phase2ProjectileWall.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Phase2ProjectileWall : MonoBehaviour
+{
+
+    public GameObject spawnedProj;
+    public int numberOfProj, numberOfHoles, additvePosition;
+    public float spacingY, wallSpawnTime;
+
+    public int overridePos1, overridePos2, numberOfWalls;
+
+    public bool noHoleInWall;
+
+    int positionIndex, lastOverride1, wallBlockerIndex;
+
+    float wallSpawnTimer;
+
+    // Use this for initialization
+
+    void Start()
+    {
+        positionIndex = overridePos1;
+        lastOverride1 = overridePos1;
+    }
+
+    void Update()
+    {
+        if (lastOverride1 != overridePos1)
+        {
+            positionIndex = overridePos1;
+            lastOverride1 = overridePos1;
+        }
+
+        wallSpawnTimer += Time.deltaTime;
+        if (wallSpawnTimer >= wallSpawnTime)
+        {
+            overridePos1 += additvePosition;
+            if (!noHoleInWall)
+            {
+                for (int i = -numberOfProj / 2; i < numberOfProj / 2 + 1; i++)
+                {
+
+                    if (i != positionIndex)
+                    {
+                        Vector3 newPos = new Vector3(transform.position.x,
+                            transform.position.y + spacingY * i);
+                        var proj = Instantiate(spawnedProj, newPos, transform.rotation);
+                        proj.transform.parent = transform;
+                        wallSpawnTimer = 0;
+                    }
+
+                    if (i == positionIndex && positionIndex < overridePos1 + numberOfHoles - 1)
+                    {
+                        positionIndex++;
+                        if (positionIndex >= overridePos1 + numberOfHoles - 1)
+                        {
+                            positionIndex = overridePos1;
+                        }
+                    }
+                }
+            }
+
+            if (noHoleInWall)
+            {
+                for (int i = -numberOfProj / 2; i < numberOfProj / 2 + 1; i++)
+                {
+                    Vector3 newPos = new Vector3(transform.position.x,
+                        transform.position.y + spacingY * i);
+                    var proj = Instantiate(spawnedProj, newPos, transform.rotation);
+                    proj.transform.parent = transform;
+                    wallSpawnTimer = 0;
+                    
+                }
+                wallBlockerIndex++;
+                if (wallBlockerIndex >= numberOfWalls)
+                {
+                    noHoleInWall = false;
+                    wallBlockerIndex = 0;
+                }
+            }
+        }
+    }
+}
---
+++ UpdatedEnemyLootDrop.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public static class UpdatedEnemyLootDrop
+{
+    static LootItem[] loot;
+    static float totalWeight;
+    static void SetUpLootTable(float duck, float pHDuck, float superDuck, float tooth, float teeth, float ammoFull, float ammoQuarter)
+    {
+        float nothing = duck + pHDuck + tooth + teeth + ammoFull + ammoQuarter;
+        loot = new LootItem[]
+        {
+            new LootItem( Resources.Load<GameObject>("LootItems/Duck_EnemyDrop"), duck),
+            new LootItem(Resources.Load<GameObject>("LootItems/P.H.Duck_EnemyDrop"), pHDuck), 
+            new LootItem(Resources.Load<GameObject>("LootItems/SuperDuck_EnemyDrop"),superDuck), 
+            new LootItem( Resources.Load<GameObject>("LootItems/Tooth"), tooth),
+            new LootItem( Resources.Load<GameObject>("LootItems/Teeth"), teeth),
+            new LootItem( Resources.Load<GameObject>("LootItems/Ammo_Full_Single"), ammoFull),
+            new LootItem( Resources.Load<GameObject>("LootItems/Ammo_Quarter_All"), ammoQuarter),
+            new LootItem( null, nothing),
+        };
+        totalWeight = TotalWeighted(duck, pHDuck, superDuck, tooth, teeth, ammoFull, ammoQuarter, nothing);
+        Array.Sort(loot);
+    }
+
+    public static GameObject FindItemToDrop(float duck, float pHDuck, float superDuck, float tooth, float teeth, float ammoFull, float ammoQuarter)
+    {
+        SetUpLootTable(duck + JP_GameManager.instance.loot.GetNormalDuckWeight, pHDuck + JP_GameManager.instance.loot.GetPhDuckWeight, 
+            superDuck + JP_GameManager.instance.loot.GetSuperDuckWeight, tooth + JP_GameManager.instance.loot.GetToothWeight, 
+            teeth + JP_GameManager.instance.loot.GetTeethWeight, ammoFull, ammoQuarter);
+        var dropNumber = Random.Range(0f, totalWeight);
+        float weighted = 0;
+
+        foreach (var item in loot)
+        {
+            weighted += item.rarity;
+            if (dropNumber <= weighted)
+            {
+                return item.item;
+            }
+            else if (weighted < 0)
+            {
+                return null;
+            }
+        }
+
+        return null;
+    }
+
+    static float TotalWeighted(float a, float b, float c,float d, float e, float f,float g, float h)
+    {
+        return a + b + c + d + e + f + g + h;
+    }
+
+}
+
+[Serializable]
+public partial class LootItem
+{
+    public GameObject item;
+    public float rarity;
+
+    public LootItem(GameObject newItem, float newRarity)
+    {
+        item = newItem;
+        rarity = newRarity;
+    }
+}
+
+public partial class LootItem : IComparable
+{
+    public int CompareTo(object obj)
+    {
+        var loot = obj as LootItem;
+        if (loot != null)
+        {
+            return rarity.CompareTo(loot.rarity);  // compare user names
+        }
+        throw new ArgumentException("Object is not a User");
+    }
+}

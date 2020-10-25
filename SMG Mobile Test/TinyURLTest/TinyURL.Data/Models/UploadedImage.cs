1746dc2ef9a1ad9a51d56359eaba929aa4e5d651
parrottjt <47727512+parrottjt@users.noreply.github.com>
2020-10-25 09:18:41 -0400

    Moving to another branch

--- SMG Mobile Test/TinyURLTest/TinyURL.Data/Models/UploadedImage.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TinyURL.Data.Models
-{
-    public class UploadedImage
-    {
-        public int Id { get; set; }
-        public string FileName { get; set; }
-        public string TinyURL { get; set; }
-    }
-}
